fb7a16945e3954e40cf60e0996c73089c1bd8f00
93814690 <93814690@qq.com>
2019-01-10 21:15:19 +0800

    add: 图片列表页添加名字搜索

--- src/main/java/top/liyf/imagehosting/ImageHostingApplication.java
+++ src/main/java/top/liyf/imagehosting/ImageHostingApplication.java
@@ -1,8 +1,14 @@
 package top.liyf.imagehosting;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.HttpMessageConverters;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.converter.HttpMessageConverter;
 
 @SpringBootApplication
 @MapperScan("top.liyf.imagehosting.dao")
@@ -12,5 +18,14 @@ public class ImageHostingApplication{
         SpringApplication.run(ImageHostingApplication.class, args);
     }
 
+    @Bean
+    public HttpMessageConverters fastJsonHttpMessageConverters() {
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.PrettyFormat);
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+        HttpMessageConverter<?> converter = fastConverter;
+        return new HttpMessageConverters(converter);
+    }
 }
 
---
+++ src/main/java/top/liyf/imagehosting/model/ImageSearchParam.java
@@ -0,0 +1,14 @@
+package top.liyf.imagehosting.model;
+
+import lombok.Data;
+
+/**
+ * @author liyf
+ * Created in 2019-01-10
+ */
+@Data
+public class ImageSearchParam {
+
+    private String name;
+
+}
