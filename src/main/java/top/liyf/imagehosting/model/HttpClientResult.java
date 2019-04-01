2e668eba88da9bec1b5dbceaf823f96380406e6f
93814690 <93814690@qq.com>
2019-04-01 22:54:35 +0800

    add: http请求相关

---
+++ src/main/java/top/liyf/imagehosting/model/HttpClientResult.java
@@ -0,0 +1,40 @@
+package top.liyf.imagehosting.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * @author liyf
+ * Created in 2019-03-31
+ */
+@Data
+public class HttpClientResult implements Serializable {
+
+    /**
+     * 响应状态码
+     */
+    private int code;
+
+    /**
+     * 响应数据
+     */
+    private String content;
+
+    public HttpClientResult() {
+    }
+
+    public HttpClientResult(int code) {
+        this.code = code;
+    }
+
+    public HttpClientResult(String content) {
+        this.content = content;
+    }
+
+    public HttpClientResult(int code, String content) {
+        this.code = code;
+        this.content = content;
+    }
+
+}
