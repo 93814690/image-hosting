3b2622b2ba254f255b4e8585ede3bda975217b85
93814690 <93814690@qq.com>
2019-04-20 15:45:09 +0800

    add: wx login

--- src/main/java/top/liyf/imagehosting/model/User.java
+++ src/main/java/top/liyf/imagehosting/model/User.java
@@ -2,6 +2,7 @@ package top.liyf.imagehosting.model;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Date;
 import java.util.HashSet;
 import java.util.Set;
@@ -11,7 +12,7 @@ import java.util.Set;
  * Created in 2018-12-19
  */
 @Data
-public class User {
+public class User implements Serializable {
 
     private Integer uid;
     private String username;
